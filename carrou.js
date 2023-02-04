63dc36dc5fef0b1cc4e80bc17d15ff971bd8fb65
francksefu <90141102+francksefu@users.noreply.github.com>
2023-02-04 18:36:10 +0200

    Add feature of slide toggle and finish with css and js file

---
+++ carrou.js
@@ -0,0 +1,26 @@
+
+const bodyy = document.body;
+but1.addEventListener('click', () => {
+    
+})
+
+$("#but1").click(function(){
+    $("#sproject1").slideDown();
+  });
+  $("#cancel1").click(function(){
+    $("#sproject1").slideUp();
+});
+
+$("#but2").click(function(){
+    $("#sproject2").slideDown();
+  });
+  $("#cancel2").click(function(){
+    $("#sproject2").slideUp();
+  });
+
+  $("#but3").click(function(){
+    $("#sproject3").slideDown();
+  });
+  $("#cancel3").click(function(){
+    $("#sproject3").slideUp();
+  });
